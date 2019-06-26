de0fdaa6ab69769175299682593f77f34990baae
Nicholas Resch <nicholasresch@gmail.com>
2019-06-26 22:24:14 +0800

    6/26 to add add employee feature

--- JSON assignment/Form1.Designer.cs
+++ JSON assignment/Form1.Designer.cs
@@ -29,16 +29,27 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.employeeTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.salaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.type = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.TextBox();
+            this.salary = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.employeeBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource1)).BeginInit();
@@ -71,57 +82,157 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Comic Sans MS", 9.857143F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.firstNameDataGridViewTextBoxColumn,
-            this.lastNameDataGridViewTextBoxColumn,
-            this.ageDataGridViewTextBoxColumn,
-            this.employeeTypeDataGridViewTextBoxColumn,
-            this.titleDataGridViewTextBoxColumn,
-            this.salaryDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.employeeBindingSource1;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 120);
+            this.dataGridView1.Location = new System.Drawing.Point(63, 176);
             this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.RowTemplate.Height = 31;
-            this.dataGridView1.Size = new System.Drawing.Size(775, 225);
+            this.dataGridView1.Size = new System.Drawing.Size(1306, 363);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // firstNameDataGridViewTextBoxColumn
+            // label2
             // 
-            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
-            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
-            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Click row to update record";
             // 
-            // lastNameDataGridViewTextBoxColumn
+            // label3
             // 
-            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
-            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
-            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "First Name";
             // 
-            // ageDataGridViewTextBoxColumn
+            // label4
             // 
-            this.ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
-            this.ageDataGridViewTextBoxColumn.HeaderText = "Age";
-            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Last Name";
             // 
-            // employeeTypeDataGridViewTextBoxColumn
+            // label5
             // 
-            this.employeeTypeDataGridViewTextBoxColumn.DataPropertyName = "EmployeeType";
-            this.employeeTypeDataGridViewTextBoxColumn.HeaderText = "EmployeeType";
-            this.employeeTypeDataGridViewTextBoxColumn.Name = "employeeTypeDataGridViewTextBoxColumn";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(711, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Age";
             // 
-            // titleDataGridViewTextBoxColumn
+            // label6
             // 
-            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
-            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
-            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(704, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Type";
             // 
-            // salaryDataGridViewTextBoxColumn
+            // label7
             // 
-            this.salaryDataGridViewTextBoxColumn.DataPropertyName = "Salary";
-            this.salaryDataGridViewTextBoxColumn.HeaderText = "Salary";
-            this.salaryDataGridViewTextBoxColumn.Name = "salaryDataGridViewTextBoxColumn";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(993, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Title";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(981, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 25);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Salary";
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(474, 51);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(166, 29);
+            this.firstName.TabIndex = 10;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(474, 109);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(166, 29);
+            this.lastName.TabIndex = 11;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(773, 51);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(166, 29);
+            this.age.TabIndex = 12;
+            // 
+            // type
+            // 
+            this.type.Location = new System.Drawing.Point(773, 112);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(166, 29);
+            this.type.TabIndex = 13;
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(1062, 54);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(166, 29);
+            this.title.TabIndex = 14;
+            // 
+            // salary
+            // 
+            this.salary.Location = new System.Drawing.Point(1062, 111);
+            this.salary.Name = "salary";
+            this.salary.Size = new System.Drawing.Size(166, 29);
+            this.salary.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1262, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 58);
+            this.button2.TabIndex = 16;
+            this.button2.Tag = "updateInfo";
+            this.button2.Text = "Update Information";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1262, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 58);
+            this.button3.TabIndex = 17;
+            this.button3.Tag = "addRecord";
+            this.button3.Text = "Add Record";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // employeeBindingSource1
             // 
@@ -131,7 +242,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1542, 559);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.salary);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -150,13 +276,22 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn employeeTypeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn salaryDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource employeeBindingSource1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.TextBox type;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.TextBox salary;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- JSON assignment/Form1.cs
+++ JSON assignment/Form1.cs
@@ -18,6 +18,7 @@ namespace JSON_assignment
 {
     public partial class Form1 : Form
     {
+        DataTable table = new DataTable();
         public Form1()
         {
             InitializeComponent();
@@ -28,38 +29,72 @@ namespace JSON_assignment
             openFileDialog1.ShowDialog();
             string pathJSON = openFileDialog1.FileName;
             string JSONdata = File.ReadAllText(pathJSON);
-            //parseJSON(pathJSON);
-
-            //using Employee;
-            //
+            // send JSON string to be parsed
             var employee = Employee.FromJson(JSONdata);
 
 
-            DataTable table = new DataTable();
-            DataView view;
+            //DataTable table = new DataTable();
+            //DataView view;
 
+            table.Columns.Add("First Name");
+            table.Columns.Add("Last Name");
+            table.Columns.Add("Age");
+            table.Columns.Add("Type");
+            table.Columns.Add("Title");
+            table.Columns.Add("Salary");
+
+            // populate rows with data from classes
             foreach (var emp in employee)
            {
                table.Rows.Add(emp.FirstName, emp.LastName, emp.Age, emp.EmployeeType, emp.Title, emp.Salary);
-
            }
 
-
-            view = new DataView(table);
-            dataGridView1.DataSource = view;
-
+            dataGridView1.DataSource = table;
         }
 
-        public void parseJSON(string path)
+
+        int indexRow;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            // click on row to display info in text boxes
+            indexRow = e.RowIndex;
+            DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+            firstName.Text = row.Cells[0].Value.ToString();
+            lastName.Text = row.Cells[1].Value.ToString();
+            age.Text = row.Cells[2].Value.ToString();
+            type.Text = row.Cells[3].Value.ToString();
+            title.Text = row.Cells[4].Value.ToString();
+            salary.Text = row.Cells[5].Value.ToString();
 
         }
 
-        public void addRows()
+        private void button2_Click(object sender, EventArgs e)
         {
-
-
+            // update information on click
+            DataGridViewRow newDataRow = dataGridView1.Rows[indexRow];
+            newDataRow.Cells[0].Value = firstName.Text;
+            newDataRow.Cells[1].Value = lastName.Text;
+            newDataRow.Cells[2].Value = age.Text;
+            newDataRow.Cells[3].Value = type.Text;
+            newDataRow.Cells[4].Value = title.Text;
+            newDataRow.Cells[5].Value = salary.Text;
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            table = dataGridView1.DataSource as DataTable;
+            DataRow newDataRow = table.NewRow();
+
+            newDataRow[0] = firstName.Text;
+            newDataRow[1] = lastName.Text;
+            newDataRow[2] = age.Text;
+            newDataRow[3] = type.Text;
+            newDataRow[4] = title.Text;
+            newDataRow[5] = salary.Text;
+
+            table.Rows.Add(newDataRow);
+
+        }
     }
 }
